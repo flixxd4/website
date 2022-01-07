4f446c7d56300be733b8785bd96e03f53acc56bc
flixxd4 <97230549+flixxd4@users.noreply.github.com>
2022-01-07 21:54:38 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const menu = document.querySelector("#mobile-menu")
+const menuLinks = document.querySelector(".navbar__menu")
+
+const mobileMenu = () => {
+    menu.classList.toggle("is-active")
+    menuLinks.classList.toggle("active")
+}
+
+menu.addEventListener("click", mobileMenu);
+
+function fill_heart() {
+    document.getElementById("heart").className = "fas fa-heart";
+
+}
